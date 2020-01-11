f8c207ed081ca9bd40ab23f3c4661675ad4ee6d2
Maciek Boniaszczuk <maciek6001@gmail.com>
2020-01-11 11:26:54 +0100

    feat: js fetch github repos api

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,42 @@
 "use strict";
 
-// service worker registration - remove if you're not going to use it
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
-// place your code below
-
 
 console.log(`Hello world!`);
 
+const list = document.querySelector('.projects-list--js');
 
+fetch('https://api.github.com/users/maciejboniaszczuk/repos?sort=updated&direction=desc')
+  .then(resp => resp.json())
+  .then(resp => {
+    const repos = resp;
+    for (const repo of repos) {
+      const {
+        description,
+        homepage,
+        html_url,
+        name
+      } = repo;
+      console.log(repo);
+      list.innerHTML += `
+      <li class="project">
+        <div class="project__container">
+           <img class="project__logo" src="../assets/img/github.svg" alt="">
+           <h3 class="project__title">${name}</h3>
+           ${
+             description ? `<p class="project__description">${description} - ${homepage}</p>` : 'No description 😞 '
+           }
+          
+        </div>
+        <div class="project__footer">
+        ${
+          homepage ? `<a class="project__link project__link--demo" href="${homepage}" target="_blank" rel="nofollow noreferrer" title="Demo: ${name}.">Demo</a>` : ''
+        }
+            <a class="project__link project__link--code" href="${html_url}" target="_blank" rel="nofollow noreferrer" title="Source code: ">GitHub</a>
+        </div>
+      </li>
+  `;
+    }
+  })
+  .catch(err => {
+    console.log(err);
+  })
